8be38e533c6bdbba7a2a4c8206cc1668fb8e3e7e
marcomini0 <marcomini330@gmail.com>
2025-10-10 11:01:36 -0300

    Add files via upload

--- TP_FINAL/TP_FINAL__AVANCES1.js
+++ TP_FINAL/TP_FINAL__AVANCES1.js
@@ -1,13 +1,14 @@
-//Buscar la forma de  hacer que los botones de la pantalla dos te lleven a nuevas otras. Hacer  que se lea el texto de la pantalla dos en adelante (Es tan largo que excede el tamaño de la pantalla, pero esta bien el tamaño de la fuente)
-
-
+//let suspenso, click;
 let imagen=[];
 let estado=0;
-let frases=[' “La sonrisa” - Ray Bradbury', 'Un niño llamado Tom \nespera en una fila en la madrugada,\nentre ruinas y observando a la gente.', 'De repente, lo intentan echar de\nla fila.', 'Tom abandona el lugar, triste.', 'Un hombre salta a defender\na Tom.', 'Él le explica a Tom que en los festivales,\n los humanos, se organizan para\n destruir obras de arte atiguas.', 'Le menciona ejemplos de\n festivales pasados.', 'No le gusta el acto de odio \n y vuelve a su casa.', 'La multitud se prepara \n para atacar la obra.', 'Frente al retrato, Tom \n se abstiene de escupir.', 'La multitud arrasa la obra, destrozandola.\n Tom rescata una parte del lienzo.', 'La multitud lo ve y lo tratan de ladrón.\n Se lo arrebatan y lo destrozan.', '  Tom corre y llega a su casa. ', 'La comparte con su familia y amigos\n y les enseña a valorarla.', 'Acostado, contempla la\n pequeña pieza de arte.', 'La pieza se convierte en un\n simbolo y la gente comienza a\n rescatar mas fragmentos.'];
+let frases=[' “La sonrisa” - Ray Bradbury', 'Un niño llamado Tom \nespera en una fila en la madrugada,\nentre ruinas y observando a la gente.', 'De repente, lo intentan echar de\nla fila.', 'Tom abandona el lugar, triste.', 'Un hombre salta a defender\na Tom.', 'Él le explica a Tom que en los festivales,\n los humanos, se organizan para\n destruir obras de arte atiguas.', 'Le menciona ejemplos de\n festivales pasados.', 'No le gusta el acto de odio \n y vuelve a su casa.', 'La multitud se prepara \n para atacar la obra.', 'Frente al retrato, Tom \n se abstiene de escupir.', 'La multitud arrasa la obra, destrozandola.\n Tom rescata una parte del lienzo.', 'La multitud lo ve y lo tratan de ladrón.\n Se lo arrebatan y lo destrozan.', '  Tom corre y llega a su casa. ', 'La comparte con su familia y amigos\n y les enseña a valorarla.', 'Acostado, contempla la\n pequeña pieza de arte.', 'La pieza se convierte en un\n simbolo y la gente comienza a\n rescatar mas fragmentos.', 'Trabajo realizado por:\n Mini Marco (122798/8)\n Macías Ignacio (122768/2 \nImagenes generadas con Gemini (IA)' ];
+
 function preload() {
   for (let NUM=0; NUM<8; NUM++) {
     imagen[NUM]=loadImage('data/a'+NUM+'.png');
   }
+  //suspenso = loadSound ("Suspenso.mp3");
+  //click = loadSound ('Click.mp3');
 }
 
 function setup() {
@@ -22,6 +23,7 @@ function draw() {
 
   if (estado===0) {
     fondo(imagen[0], frases[0], 48, width/2, height/4, 220, 330, 200, 50, "COMENZAR");
+    boton(250, 270, 140, 50, "Créditos");
   }
 
   if (estado===1) {
@@ -87,116 +89,132 @@ function draw() {
   if (estado===15) {
     fondo(imagen[4], frases[15], 40, width/2, height/4, 250, 370, 150, 50, "Reiniciar" );
   }
+
+  if (estado===16) {
+    fondo(imagen[0], frases[16], 40, width/2, height/4, 40, 400, 150, 50, "Volver" );
+  }
 }
 
 
 function mouseClicked() {
+ 
+
   if (estado===0) {
-    if (overMouse(250, 330, 200, 50)) {
+    if (sobreMouse(250, 330, 200, 50)) {
       estado=1;
     }
   }
   if (estado===1) {
-    if (overMouse( 250, 370, 140, 50)) {
+    if (sobreMouse( 250, 370, 140, 50)) {
       estado=2;
     }
   }
 
   if (estado===2) {
-    if (overMouse( 120, 370, 140, 50)) {
+    if (sobreMouse( 120, 370, 140, 50)) {
       estado=3;
     }
-    if (overMouse( 370, 370, 140, 50)) {
+    if (sobreMouse( 370, 370, 140, 50)) {
       estado=4
     }
   }
 
   if (estado===3) {
-    if (overMouse( 250, 330, 150, 50)) {
+    if (sobreMouse( 250, 330, 150, 50)) {
       estado=0;
     }
   }
 
   if (estado===4) {
-    if (overMouse(  250, 370, 150, 50)) {
+    if (sobreMouse(  250, 370, 150, 50)) {
       estado=5;
     }
   }
 
   if (estado===5) {
-    if (overMouse( 160, 330, 320, 50)) {
+    if (sobreMouse( 160, 330, 320, 50)) {
       estado=6;
     }
   }
 
   if (estado===6) {
-    if (overMouse( 70, 370, 250, 50)) {
+    if (sobreMouse( 70, 370, 250, 50)) {
       estado=7;
     }
-    if (overMouse(330, 370, 250, 50)) {
+    if (sobreMouse(330, 370, 250, 50)) {
       estado=8;
     }
   }
 
   if (estado===7) {
-    if (overMouse( 250, 330, 150, 50)) {
+    if (sobreMouse( 250, 330, 150, 50)) {
       estado=0;
     }
   }
   if (estado===8) {
-    if (overMouse(  250, 370, 150, 50)) {
+    if (sobreMouse(  250, 370, 150, 50)) {
       estado=9;
     }
   }
   if (estado===9) {
-    if (overMouse(  220, 330, 200, 50)) {
+    if (sobreMouse(  220, 330, 200, 50)) {
       estado=10;
     }
   }
 
   if (estado===10) {
-    if (overMouse( 330, 370, 270, 50)) {
+    if (sobreMouse( 330, 370, 270, 50)) {
       estado=11;
     }
-    if (overMouse(70, 370, 220, 50)) {
+    if (sobreMouse(70, 370, 220, 50)) {
       estado=12;
     }
   }
 
   if (estado===11) {
-    if (overMouse(  220, 330, 200, 50)) {
+    if (sobreMouse(  220, 330, 200, 50)) {
       estado=0;
     }
   }
   if (estado===12) {
-    if (overMouse( 290, 330, 340, 50)) {
+    if (sobreMouse( 290, 330, 340, 50)) {
       estado=14;
     }
-    if (overMouse(20, 330, 260, 50)) {
+    if (sobreMouse(20, 330, 260, 50)) {
       estado=13;
     }
   }
 
   if (estado===13) {
-    if (overMouse(  250, 330, 150, 50)) {
+    if (sobreMouse(  250, 330, 150, 50)) {
       estado=0;
     }
   }
 
   if (estado===14) {
-    if (overMouse( 250, 330, 150, 50)) {
+    if (sobreMouse( 250, 330, 150, 50)) {
       estado=15;
     }
   }
-  
-   if (estado===15) {
-    if (overMouse( 250, 370, 150, 50)) {
+
+  if (estado===15) {
+    if (sobreMouse( 250, 370, 150, 50)) {
+      estado=0;
+    }
+  }
+  if (estado===0) {
+    if (sobreMouse( 255, 270, 140, 50)) {
+      estado=16;
+    }
+  }
+  if (estado===16) {
+    if (sobreMouse(40, 400, 150, 50)) {
       estado=0;
     }
   }
 }
 function boton(posX, posY, tamX, tamY, textoBoton) {
-  if (overMouse(posX, posY, tamX, tamY)) {
+  if (sobreMouse(posX, posY, tamX, tamY)) {
     fill(255, 50, 0);
   } else {
     fill(200, 0, 0, 100);
@@ -214,15 +232,12 @@ function fondo(imagen, texto, textsize, posX, posY, posXB, posYB, tamXB, tamYB, 
   image(imagen, 0, 0);
   fill(255);
   textAlign(CENTER, TOP);
-  //textFont('CastlePressNo2');
-  // textSize(textsize);
   text(texto, posX, posY);
   textSize(30);
   boton(posXB, posYB, tamXB, tamYB, textoBoton);
 }
 
 
-
-function overMouse(posX, posY, tamX, tamY) {
+function sobreMouse(posX, posY, tamX, tamY) {
   return mouseX>posX && mouseX<posX+tamX && mouseY>posY && mouseY<posY+tamY;
 }
